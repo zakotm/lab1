d4d323eb8f21aefd38fe720e80e3b0539f89509a
Michael Stecklein <michaelrstecklein@gmail.com>
2016-01-28 16:06:18 -0600

    Label validation function done.

--- assembler.c
+++ assembler.c
@@ -18,6 +18,48 @@ UTEID 2:
 
 
 
+
+
+/*
+	Symbol Table definition:
+*/
+#define MAX_LABEL_LEN 20
+#define MAX_SYMBOLS 255
+
+typedef struct {
+	int address;
+	char label[MAX_LABEL_LEN + 1];
+} Label;
+
+int symbolTableSize = 0;
+Label symbolTable[MAX_SYMBOLS];
+
+/* debugging method */
+void printSymbolTable() {
+	printf("+++++++++++Symbol Table++++++++++\n");
+	int i;
+	for (i = 0; i < symbolTableSize; i++) {
+		printf(	"0x%.4X\t%s\n",
+				symbolTable[i].address,
+				symbolTable[i].label);
+	}
+	printf("+++++++++++++++++++++++++++++++++\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 	Input: String to determine if it is a name of an opcode in LC-3b assembly and to return the respective opcode value.
 	Output: The numerical opcode, or -1 if not an opcode name
@@ -40,12 +82,47 @@ int isValidPseudoOp(const char* pseudoopName) {
 
 
 /*
-	Input: String to determine if it is a name of a valid label name
+	Input: Lowercase string to determine if it is a name of a valid label name
 	Output: 0 if not a valid label name, 1 if is valid
 */
 int isValidLabel(const char* labelName) {
-	printf("WARNING: isValidLabel() is not yet implemented\n");
-	return 0;
+	if (!labelName) { return 0; } /* null pointer */
+
+	if (strlen(labelName) == 0 || strlen(labelName) > 20) { return 0; } /* must be of length 1 to 20 */
+
+	if (labelName[0] == 'x') { return 0; } /* cannot start with 'x' */
+
+	if (strcmp(labelName,"orig")==0 || strcmp(labelName,"fill")==0 ||
+			strcmp(labelName,"end")==0) { return 0; } /* cannot be opcode name*/
+
+	if (getOpcode(labelName) != -1) { return 0; } /* cannot be an operation name */
+
+	if (isdigit(labelName[0])) { return 0; } /* cannot start with a digit */
+
+	if (strcmp(labelName,"in")==0 || strcmp(labelName,"out")==0 || strcmp(labelName,"getc")==0
+			|| strcmp(labelName,"puts")==0) { return 0; } /* cannot be IN, OUT, GETC, or PUTS */
+
+	/* cannot be a register name */
+	int i;
+	for (i = 0; i < 8; i++) {
+		char reg[] = "R ";
+		reg[1] = i + 48; /* 0 is 48 is ASCII */
+		if (strcmp(labelName, reg) == 0) {
+			return 0;
+		}
+	}
+
+	/* must be made of alphanumeric characters */
+	i = 0;
+	while (labelName[i] != 0) {
+		if (!isalnum(labelName[i])) {
+			return 0;
+		}
+		i++;
+	}
+
+	/* passes all tests, is valid label */
+	return 1;
 }
 
 
@@ -230,17 +307,6 @@ int strToNum( char * pStr )
 
 
 
-
-#define MAX_LABEL_LEN 20
-#define MAX_SYMBOLS 255
-
-typedef struct {
-	int address;
-	char label[MAX_LABEL_LEN + 1];
-} Label;
-
-int symbolTableSize = 0;
-Label symbolTable[MAX_SYMBOLS];
  
 
 
@@ -292,32 +358,33 @@ int main(int argc, char* argv[]) {
 	/*
 		First Pass to Build Symbol Table
 	*/
-	int lineCount = 1;
+	int opCount = 0;
 	do
 	{
 		lRet = readAndParse( infile, lLine, &lLabel, &lOpcode, &lArg1, &lArg2, &lArg3, &lArg4 );
+		
 		if( lRet != DONE && lRet != EMPTY_LINE )
 		{
-			if (strcmp(lLabel,"")) { /* if label is not blank */
-				if (isValidLabel(lLabel)) {
-					if (getLabelAddress(lLabel) == -1) {
-						strcpy(symbolTable[symbolTableSize].label, lLabel);
-						symbolTable[symbolTableSize].address = startAddress + 2*lineCount;
-						symbolTableSize++;
-					} else {
-						exit(4);
-					}
+			if (strcmp(lLabel,"")) { /* if label is blank, there is no label on this line of the sourcecode */
+				if (isValidLabel(lLabel)  &&  getLabelAddress(lLabel) == -1) {
+
+					strcpy(symbolTable[symbolTableSize].label, lLabel);
+					symbolTable[symbolTableSize].address = startAddress + 2*opCount;
+					symbolTableSize++;
+
 				} else {
+					/* invalid label or label is already in the table */
+					printf("EXITING: %s     %i\n",lLabel,isValidLabel(lLabel));
 					exit(4);
 				}
 			}
 
-			if (getOpcode(lOpcode) != -1) { /* lineCount only increments for each operations */
-				lineCount++;
+			if (getOpcode(lOpcode) != -1) { /* lineCount only increments for each operation */
+				opCount++;
 			}
 		}
 	} while( lRet != DONE );
-
+	printSymbolTable();
 
 
 	/*
