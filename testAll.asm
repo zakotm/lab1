6b032b8eed5842687e7b8d6af98b1475928b8ef8
zakotm <Zakotm@wireless-10-148-171-23.public.utexas.edu>
2016-02-01 11:38:01 -0600

    testAll case fix

--- testAll.asm
+++ testAll.asm
@@ -2,17 +2,17 @@
         ADD R1, R1, R0
 	AND R1, R1, R2
 	BR Y
-	BRn A
-	BRnz A
-	BRnp A
-	BRp A
-	BRpz A
-	BRz A
-	BRnzp A
+	BRNC A
+	BRNZ A
+	BRNP A
+	BRP A
+	BRZP A
+	BRZ A
+	BRNZP A
 	HALT
 	JMP R1
 	JSR Y
-	JSSR R1
+	JSRR R1
 	LDB R1, R4, #5
 	LEA R0, A
 	NOP 
@@ -23,11 +23,11 @@
 	RSHFA R2, R3, #3
 	RTI
 	STB R4, R2, #10
-	STW R4, R2, #10
+A	STW R4, R2, #10
 	TRAP x23
 	XOR R3, R1, R2
  	XOR R3, R1, #12
-A	LEA R1, Y
+	LEA R1, Y
 	LDW R1, R1, #0
 	LDW R1, R1, #0
 	ADD R1, R1, R1
