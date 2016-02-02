21b6a6ac550e78b54734d20f71917ba1ea9d3612
Michael Stecklein <michaelrstecklein@gmail.com>
2016-02-01 21:27:08 -0600

    Shuffle errors fixedgit diff!

--- shuffle.asm
+++ shuffle.asm
@@ -6,17 +6,18 @@
 	; R3 will hold the base address (x4000) of the old data
 	; R4 will hold the base address (x4005) of the destination
 
-	AND R0, R0, #0      ; clear R0
-	LEA R0, MASK        ; get the mask address
-	LDB R0, R0, #0	    ; load the mask into R0
+    AND R0, R0, #0      ; clear R0 
+    LEA R0, MASK        ; get the address of the  mask address
+    LDW R0, R0, #0      ; load the address of the mask
+    LDW R0, R0, #0      ; load the mask into R0 
 
 	AND R3, R3, #0	    ; clear
 	LEA R3, OLD 		; get address
-	LDB R3, R3, #0	    ; load x4000 into R3
+	LDW R3, R3, #0	    ; load x4000 into R3
 
 	AND R4, R4, #0	    ; clear
 	LEA R4, DEST	    ; get address
-	LDB R4, R4, #0	    ; load x4005 into R4
+	LDW R4, R4, #0	    ; load x4005 into R4
 
 	; Byte 0x4005
 	AND R1, R1, #0      ; clear R1
@@ -27,24 +28,24 @@
 
 	; Byte 0x4006
 	AND R1, R1, #0      ; clear R1
-	RSHFL R1, R1, #2	; shift bits to least significant positions
-	AND R1, R0, #3      ; AND mask with 0000 0011 to get the bit[0:1]
+	RSHFL R1, R0, #2	; shift bits to least significant positions
+	AND R1, R1, #3      ; AND mask with 0000 0011 to get the bit[0:1]
 	ADD R1, R1, R3		; The value got will be added to 0x4000 (OLD), R1 now holds the address to read from
 	LDB R1, R1, #0		; load the value at address into R1
 	STB R1, R4, #1		; store R1 into x4006
 
 	; Byte 0x4007
 	AND R1, R1, #0      ; clear R1
-	RSHFL R1, R1, #4	; shift bits to least significant positions
-	AND R1, R0, #3      ; AND mask with 0000 0011 to get the bit[0:1]
+	RSHFL R1, R0, #4	; shift bits to least significant positions
+	AND R1, R1, #3      ; AND mask with 0000 0011 to get the bit[0:1]
 	ADD R1, R1, R3		; The value got will be added to 0x4000 (OLD), R1 now holds the address to read from
 	LDB R1, R1, #0		; load the value at address into R1
 	STB R1, R4, #2		; store R1 into x4007
 
 	; Byte 0x4008
 	AND R1, R1, #0      ; clear R1
-	RSHFL R1, R1, #6	; shift bits to least significant positions
-	AND R1, R0, #3      ; AND mask with 0000 0011 to get the bit[0:1]
+	RSHFL R1, R0, #6	; shift bits to least significant positions
+	AND R1, R1, #3      ; AND mask with 0000 0011 to get the bit[0:1]
 	ADD R1, R1, R3		; The value got will be added to 0x4000 (OLD), R1 now holds the address to read from
 	LDB R1, R1, #0		; load the value at address into R1
 	STB R1, R4, #3		; store R1 into x4008
@@ -58,6 +59,7 @@ DEST    .FILL x4005
 
 
         .END  ;The pseudo-op, delimiting the source program
+
 
 
 
